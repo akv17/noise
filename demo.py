3f6cc23b129af4c0137dbe8771728ff54e9afecf
akv17 <temakep@list.ru>
2020-09-08 21:34:53 +0300

    upd demo with palette

--- demo.py
+++ demo.py
@@ -1,8 +1,10 @@
 from noise.core import Scene
 from noise.shapes import PerlinGrid
+from noise.colors import GradientPalette
 
 W, H = 1200, 600
 GW, GH = 40, 40
 scene = Scene(W, H)
-grid = PerlinGrid(GW, GH)
+palette = GradientPalette(src=(19, 91, 117), dst=(25, 168, 128))
+grid = PerlinGrid(GW, GH, palette=palette, draw_edges=True)
 scene.animate(shape=grid)
