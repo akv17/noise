1a6e8207623bd722db16350e1f1ea8af5a86cf72
akv17 <temakep@list.ru>
2020-09-09 22:12:06 +0300

    fix fn

--- examples/water.py
+++ examples/water.py
@@ -4,4 +4,4 @@ from noise.scenes import Water
 view = View(width=1200, height=600)
 scene = Water(width=50, height=50)
 film = view.animate(scene, n_frames=300, size=(1200, 600))
-film.to_gif('w.gif')
+film.to_gif('water.gif')
