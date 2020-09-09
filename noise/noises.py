14b3c708554f5bda59e2f334cb25eb19124bbfab
akv17 <temakep@list.ru>
2020-09-09 21:48:52 +0300

    add noise obj, pkg

---
+++ noise/noises.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+
+import numpy as np
+from opensimplex import OpenSimplex
+
+PERLIN_NOISE = OpenSimplex().noise2d
+
+
+class BaseNoise:
+
+    @abstractmethod
+    def apply(self, verts):
+        raise NotImplementedError
+
+
+class DummyNoise(BaseNoise):
+
+    def apply(self, verts):
+        return verts
+
+
+class PerlinNoise(BaseNoise):
+
+    def __init__(self, speed=0.1, scale=0.2):
+        self.speed = speed
+        self.scale = scale
+        self._offset = 0
+
+    def _compute_vertex(self, vertex):
+        x, y, _ = vertex
+        z = PERLIN_NOISE(x * self.scale + self._offset, y * self.scale + self._offset)
+        return [x, y, z]
+
+    def apply(self, verts):
+        verts = np.apply_along_axis(self._compute_vertex, 1, verts)
+        self._offset -= self.speed
+        return verts
