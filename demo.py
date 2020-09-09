c153b7f70cf94dc8029f6516f0db06f078a27327
akv17 <temakep@list.ru>
2020-09-09 21:58:39 +0300

    rename classes

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
-from noise.core import Scene
-from noise.shapes import Grid
+from noise.core import View
+from noise.scenes import Grid
 from noise.noises import PerlinNoise
 from noise.colors import GradientPalette
 
 
-scene = Scene(width=1200, height=600)
+view = View(width=1200, height=600)
 noise = PerlinNoise(speed=0.05)
 palette = GradientPalette(src=(6, 44, 71), dst=(27, 86, 128))
 grid = Grid(width=50, height=50, noise=noise, palette=palette, draw_edges=True)
-scene.ianimate(grid)
+view.ianimate(grid)
